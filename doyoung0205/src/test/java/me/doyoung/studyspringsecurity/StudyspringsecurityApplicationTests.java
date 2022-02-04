af34ebc77b5fa4cd296455c83491b6b2516cb56a
김도영 <doyoung0205@naver.com>
2022-02-04 10:13:13 +0900

    add : doyoung project

---
+++ doyoung0205/src/test/java/me/doyoung/studyspringsecurity/StudyspringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package me.doyoung.studyspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyspringsecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
