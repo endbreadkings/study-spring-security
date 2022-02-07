16576ba274f710449aff14d3cdb53a64fa762859
haedoang <haedoang@gmail.com>
2022-02-07 12:17:30 +0900

    feat: basic auth
    - basic auth
    - use antMachters

---
+++ haedoang/demo/src/main/java/kr/co/haedoang/demo/security/ApplicationSecurityConfig.java
@@ -0,0 +1,29 @@
+package kr.co.haedoang.demo.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * packageName : kr.co.haedoang.demo.security
+ * fileName : ApplicationSecurityConfig
+ * author : haedoang
+ * date : 2022-02-07
+ * description : Basic Auth
+ */
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "index", "/css/*", "/js/*")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+}
