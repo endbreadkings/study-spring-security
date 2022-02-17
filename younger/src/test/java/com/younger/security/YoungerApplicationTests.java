02ab281898e32e70a3e36647cd2b8d9e893d7b9c
younger-k <younger9403@gmail.com>
2022-02-17 23:49:03 +0900

    add: younger project

---
+++ younger/src/test/java/com/younger/security/YoungerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.younger.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoungerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
